006213862ba4d6cc5999bb27b487b0aa2ae39894
mandboli18 <00001097993024sp@al.educacao.sp.gov.br>
2024-11-27 20:14:04 -0300

    Add files via upload
    .

---
+++ perguntas.js
@@ -0,0 +1,34 @@
+criaCartao(
+    'Música',
+    'Qual é o nome do famoso cantor conhecido como "Rei do Pop?',
+    'Michael Jackson'
+)
+
+criaCartao(
+    'Literatura',
+    'O livro mais famoso de Antoine de Saint-Exupéry é ( O Pequeno príncipe) VERDADEIRO ou FALSO',
+    'Verdadeiro'
+)
+
+criaCartao(
+    'Animação',
+    'Qual é o trabalho de Bob Esponja?',
+    'Chef de cozinha'
+)
+
+criaCartao(
+    'Filosofia',
+    'Quem é considerado o pai da filosofia ocidental?',
+    'Sócrates'
+)
+criaCartao(
+    'Literatura',
+    'Quem escreveu Dom Casmurro?',
+    'Machado de Assis'
+)
+
+criaCartao(
+    'Espaço Sideral',
+    'Qual é o planeta vermelho do nosso sistema solar?',
+    'Marte'
+)
